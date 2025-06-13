c9b821468c1fc1bfa8ad96c355509e7d12070cf4
Shivank Sharma <shivanksharma070@gmail.com>
2025-06-13 10:44:16 +0530

    Setup tcp listner and accept connection

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net"
+)
+
+const default_listen_Addr = ":5001"
+
+type Config struct {
+	listenAddr string
+}
+type Server struct {
+	Config
+	ln          net.Listener
+	Peers       map[*Peer]bool // For managing connections
+	addPeerChan chan *Peer
+	quitChan    chan struct{}
+	msgChan     chan []byte
+}
+
+func NewServer(config Config) *Server {
+	if len(config.listenAddr) == 0 {
+		config.listenAddr = default_listen_Addr
+	}
+
+	return &Server{
+		Config:      config,
+		Peers:       make(map[*Peer]bool),
+		addPeerChan: make(chan *Peer),
+		quitChan:    make(chan struct{}),
+		msgChan:     make(chan []byte),
+	}
+}
+
+// Listner for tcp connection
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.listenAddr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+
+	go s.Loop()
+	slog.Info("Server running", "listenAddr", s.listenAddr)
+	return s.acceptLoop()
+}
+
+// Manage connections
+func (s *Server) Loop() {
+	for {
+		select {
+		case rawMsg := <-s.msgChan:
+			fmt.Println(string(rawMsg))
+		case <-s.quitChan:
+			return
+		case peer := <-s.addPeerChan:
+			s.Peers[peer] = true
+		}
+	}
+}
+
+// Accepting Connection
+func (s *Server) acceptLoop() error {
+	for {
+		conn, err := s.ln.Accept() // Blocking
+		if err != nil {
+			slog.Error("Accept error", "err", err)
+			continue
+		}
+		go s.handleConnection(conn)
+	}
+}
+
+// Handling Connections
+func (s *Server) handleConnection(conn net.Conn) {
+	peer := NewPeer(conn, s.msgChan)
+	s.addPeerChan <- peer
+	slog.Info("Peer conntected", "connection", conn.LocalAddr())
+	err := peer.readLoop()
+	if err != nil {
+		slog.Error("Peer read error", "err", err)
+	}
+}
+
+func main() {
+	server := NewServer(Config{})
+	log.Fatal(server.Start())
+}
---
+++ peer.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net"
+)
+
+type Peer struct {
+	conn  net.Conn
+	msgCh chan []byte
+}
+
+func NewPeer(conn net.Conn, msgChan chan []byte) *Peer {
+	return &Peer{
+		conn:  conn,
+		msgCh: msgChan,
+	}
+}
+
+func (p *Peer) readLoop() error {
+	// Reading data form a connection
+	buf := make([]byte, 1024)
+	for {
+		n, err := p.conn.Read(buf)
+		if err != nil {
+			return err
+		}
+
+		msgBuf := make([]byte, n)
+		copy(msgBuf, buf)
+		p.msgCh <- msgBuf
+	}
+}
